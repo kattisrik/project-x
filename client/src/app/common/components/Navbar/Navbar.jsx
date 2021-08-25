c7f4c1937a1d9a3eb6b8f6898884a78645ca5001
Darshan P B <dpb@synamedia.com>
2021-08-25 22:05:19 +0530

    draft nav

--- client/src/app/common/components/Navbar/Navbar.jsx
+++ client/src/app/common/components/Navbar/Navbar.jsx
@@ -17,22 +17,43 @@ const PAGES = [
         url: '/about',
         title: 'About'
     },
-    {
-        url: '/contact',
-        title: 'Contact Us'
-    }
+    // {
+    //     url: '/contact',
+    //     title: 'Contact Us'
+    // }
 ]
 
 const Navbar = ({ children }) => {
     return (
         <div>
-            <AppBar position='static'>
+            {/* <AppBar position='static'>
                 <Tabs>
                     { PAGES.map((page, index) => {
                         return <Tab label={ page.title } href={ page.url } key={ index } />
                     }) }
                 </Tabs>
-            </AppBar>
+            </AppBar> */}
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/home">Project X</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            { PAGES.map((page, index) => {
+                                return <li class="nav-item">
+                                    <a class="nav-link" aria-current="page" href={ `${page.url}` }>{ page.title }</a>
+                                </li>;
+                            }) }
+                        </ul>
+                        {/* <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form> */}
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 };
--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import dynamic from "next/dynamic";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Paper, Container } from "@material-ui/core";
-
+import 'bootstrap/dist/css/bootstrap.css';
 import theme from 'theme';
 const Navbar = dynamic(
   import("components/Navbar")
@@ -12,8 +12,10 @@ const ProjectX = ({ Component, pageProps }) => {
     <ThemeProvider theme={ theme }>
       <Paper style={ { height: "100vh" } }>
         <Container disableGutters={ true } maxWidth={ false }>
-          <Navbar />
-          <Component { ...pageProps } />
+          <div class="container">
+            <Navbar />
+            <Component { ...pageProps } />
+          </div>
         </Container>
       </Paper>
     </ThemeProvider>
