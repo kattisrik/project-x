2de7682232714b27be734407095a656b8d02736d
Darshan P B <dpb@synamedia.com>
2021-09-04 21:03:11 +0530

    Basic layout

---
+++ client/src/app/common/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return <>
+        <footer class="text-muted py-3 " style={ { backgroundColor: "#e3f2fd" } }>
+            <div class="container">
+                <p class="float-end mb-1">
+                    <a href="#">Back to top</a>
+                </p>
+                <p class="mb-0">New to Project? <a href="https://github.com/kattisrik/project-x" target='_blank'>Visit Project Here</a> or read our <a href="https://github.com/kattisrik/project-x#readme" target='_blank'>getting started guide</a>.</p>
+            </div>
+        </footer>
+    </>
+}
+
+export default Footer;
